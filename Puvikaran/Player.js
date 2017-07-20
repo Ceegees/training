76f58d00148584c015e3ea4e5a59c09fb8e668bf
Puvikaran <puvikaran.s@gmail.com>
2017-07-20 18:32:46 +0530

    changes made with timer

--- Puvikaran/Player.js
+++ Puvikaran/Player.js
@@ -1,123 +1,146 @@
 function Player(name, id) {
+
     this.id = id;
     this.name = name;
     this.hand = [];
-    this.totalValues = 0;
-}
-addPlayer();
+    this.handValues = 0;
 
-function addPlayer() {
-
-    var playerName = ['Samuel', 'Nithin', 'Emil', 'Dhananjay', 'Godly'];
-
-    for (var i = 0; i < playerName.length; i++) {
-        var newPlayer = new Player(playerName[i], i);
-        Players.push(newPlayer);
-
-    }
-    // console.log(Players);
-}
-var noplayers = Players.length;
-if (noplayers < 6) {
-
-    distributeCard(deck);
-
-} else {
-    console.log("Enter max 6 Player");
 }
 
-function distributeCard() {
 
-    var cardMod = this.deck.length % Players.length;
-    var distCount = ((this.deck.length - cardMod) / Players.length);
+// function addCard(card) {
+//     hand.push(card);
+// }
+//
+// function removeCard(card) {
+//     if (hand.length > 0)
+//         return hand.pop();
+//     else
+//         return null;
+// }
+
+function distributeCard(shuffledCards) {
+
+    var cards = shuffledCards;
+    console.log("Cards Distributed");
+    console.log("Remaining Cards---> ", cards);
+
+    var cardMod = cards.length % this.Players.length;
+    var distCount = ((cards.length - cardMod) / this.Players.length);
 
     for (var i = 0; i < distCount; i++) {
-        for (var j = 0; j < Players.length; j++) {
-            var takenCard = takeCard();
-            Players[j].hand.push(takenCard);
-        }
+        (function(i) {
+            setTimeout(function() {
+
+                for (var j = 0; j < Players.length; j++) {
+                    var takenCard = takeCard();
+                    Players[j].hand.push(takenCard);
+                }
+                document.getElementById("displayPlayers").innerHTML = "";
+                handValue();
+
+            }, 3000 * i);
+        }(i));
     }
 
+    function takeCard() {
+        var randCard = Math.floor(Math.random() * cards.length);
+        var takenCard = cards[randCard];
+        cards.splice(randCard, 1);
 
-function takeCard() {
-    var randCard = Math.floor(Math.random() * deck.length);
-    var takenCard = deck[randCard];
-    deck.splice(randCard, 1);
+        return takenCard;
+    }
 
-    return takenCard;
 }
 
-var playersCount = Players.length;
-var sortedPlayers = undefined;
+function handValue() {
+    var playersCount = Players.length;
 
-for (var p = 0; p < playersCount; p++) {
-    for (var i = 0; i < Players[p].hand.length; i++) {
+    for (var p = 0; p < playersCount; p++) {
+        for (var i = 0; i < Players[p].hand.length; i++) {
 
-        switch (Players[p].hand[i].values) {
+            switch (Players[p].hand[i].value) {
 
-            case "2":
-                Players[p].hand[i].values = 2;
+                case "2":
+                    Players[p].hand[i].value = 2;
+                    break;
+                case "3":
+                    Players[p].hand[i].value = 3;
+                    break;
+                case "4":
+                    Players[p].hand[i].value = 4;
+                    break;
+                case "5":
+                    Players[p].hand[i].value = 5;
+                    break;
+                case "6":
+                    Players[p].hand[i].value = 6;
+                    break;
+                case "7":
+                    Players[p].hand[i].value = 7;
+                    break;
+                case "8":
+                    Players[p].hand[i].value = 8;
+                    break;
+                case "9":
+                    Players[p].hand[i].value = 9;
+                    break;
+                case "10":
+                    Players[p].hand[i].value = 10;
+                    break;
+                case "J":
+                    Players[p].hand[i].value = 11;
+                    break;
+                case "Q":
+                    Players[p].hand[i].value = 12;
+                    break;
+                case "K":
+                    Players[p].hand[i].value = 13;
+                    break;
+                case "A":
+                    Players[p].hand[i].value = 14;
+                    break;
 
-                break;
-            case "3":
-                Players[p].hand[i].values = 3;
+            }
 
-                break;
-            case "4":
-                Players[p].hand[i].values = 4;
-
-                break;
-            case "5":
-                Players[p].hand[i].values = 5;
-
-                break;
-            case "6":
-                Players[p].hand[i].values = 6;
-
-                break;
-            case "7":
-                Players[p].hand[i].values = 7;
-
-                break;
-            case "8":
-                Players[p].hand[i].values = 8;
-
-                break;
-            case "9":
-                Players[p].hand[i].values = 9;
-
-                break;
-            case "10":
-                Players[p].hand[i].values = 10;
-
-                break;
-            case "J":
-                Players[p].hand[i].values = 11;
-
-                break;
-            case "Q":
-                Players[p].hand[i].values = 12;
-
-                break;
-            case "K":
-                Players[p].hand[i].values = 13;
-
-                break;
-            case "A":
-                Players[p].hand[i].values = 14;
-
-                break;
+            Players[p].handValues += Players[p].hand[i].value;
 
         }
-
-        Players[p].totalValues += Players[p].hand[i].values;
-
+        console.log(Players[p]);
     }
-    console.log(Players[p]);
-}
-Players.sort(function(a, b) {
 
-    return a.totalValues - b.totalValues;
-});
-console.log(Players);
+    displayOnce();
+  }
+
+function displayOnce() {
+
+  var compiled = _.template($("#player-template").html(), Players);
+  $('#displayPlayers').append(compiled);
+
+  sortedPlayers();
+
+
+  _.each(Players, function(player) {
+      player.handValues = 0;
+      // console.log(player.name + " => " + player.handValues);
+      _.each(player.hand, function(handCard) {
+          handCard = 0;
+          // console.log(handCard.name);
+      });
+  });
+}
+
+function sortedPlayers() {
+    Players.sort(function(a, b) {
+
+        return a.handValues - b.handValues;
+    });
+
+    console.log("Players Sorted based on Hand Values");
+
+    _.each(Players, function(player) {
+        console.log(player);
+    });
+    Players.reverse();
+
 }
