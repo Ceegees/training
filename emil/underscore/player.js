0a4914f6ae64f67f8ca8d01acf5d2e52385542d5
Emil Yeldho <emilyeldho1992@gmail.com>
2017-08-18 21:56:27 +0530

    This is 18/8/17 commit

--- emil/underscore/player.js
+++ emil/underscore/player.js
@@ -2,8 +2,8 @@ function Player(name, id) {
   this.name = name;
   this.id = id;
   this.hand = [];
-  this.handValues = 0;
-  this.handValues = function() {
+  this.handValues=0;
+  this.handValue = function() {
     var totalvalue = 0;
     for (var i = 0; i < this.hand.length; i++) {
       var value = this.hand[i].getValue();
